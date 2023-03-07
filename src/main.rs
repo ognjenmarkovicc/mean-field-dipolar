0c9bfbaaa8d174da1763fb8dacfa3d08ea5db904
ognjenmarkovicc <ognjen.markovicc@gmail.com>
2023-03-07 12:00:03 -0500

    Calculate tunnelings for a specific pattern

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,26 @@
 extern crate nalgebra as na;
-use approx::assert_relative_eq;
-use na::{Vector3};
-
-use mean_field_dipolar::lattice::{PeriodicLattice,
-    SpinIdx, LattPos};
-use mean_field_dipolar::dipolar::{DipolarSystem, get_dd_int, get_dd_int_site, generate_dd_int_mat, generate_mat_m};
+use mean_field_dipolar::dipolar::{DipolarSystem, get_dd_int_site, generate_dd_int_mat, generate_mat_m, get_mu_inequality, get_tunneling};
 use mean_field_dipolar::patterns::get_checkerboard;
+use na::DVector;
 
 fn main() {
-    let dip_v = Vector3::new(0., 0., 1.);
-    let dist_v = Vector3::new(1., 0., 0.);
-
-    let interaction = get_dd_int(dist_v, dip_v);
-
-    println!("d-d interaction {}", interaction);
-
-    let system = PeriodicLattice::new(2);
-    let idx = 1;
-    assert_eq!(1, system.get_idx_periodic(idx));
-
-    let sp = SpinIdx::new(1, &system);
-
-    let pos = LattPos::from(sp);
-
-    println!("{:?}", pos);
-
-    let mat = get_checkerboard(&system);
-
-    println!("{:?}", mat);
-
     let mut dip_system = DipolarSystem::new(0., 0., 20., 1, 4);
-    dip_system.update_occupation(mat);
-
-    let interaction = get_dd_int_site(0, 0, &dip_system);
-
-    println!("Interaction {}", interaction);
-
+    dip_system.update_occupation(get_checkerboard(&dip_system.latt));
     generate_dd_int_mat(&mut dip_system);
+    let (lower, upper) = get_mu_inequality(&dip_system);
 
-    println!("{:?}", dip_system.dd_mat);
+    println!("Lower mu {:.2} upper mu {:.2}", lower, upper);
 
-    let t = 1.;
-    let mu = 1.;
-    let m_mat = generate_mat_m(mu, t, &dip_system);
+    if lower < upper {
+        let no_points = 100;
+        let delta_mu = (upper - lower)/(no_points as f64);
+        let mu_vals = DVector::from_iterator(no_points, (0..no_points).map(|idx| (idx as f64)*delta_mu + lower));
 
-    println!("{:?}", m_mat);
+        let tunneling = DVector::from_iterator(no_points, 
+                                               mu_vals.iter()
+                                                      .map(|mu| get_tunneling(*mu, &dip_system,
+                                                                              1.,  1e-3, 1e-2)));
+        println!("{:?}", tunneling);
+    }
+
 }
