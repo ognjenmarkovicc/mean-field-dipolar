42e391c70b433b957e49067cae59169f52490233
ognjenmarkovicc <ognjen.markovicc@gmail.com>
2023-03-09 21:27:34 -0500

    Add fully filled and horizontal stripe patterns

--- src/patterns.rs
+++ src/patterns.rs
@@ -14,3 +14,11 @@ pub fn get_checkerboard(latt: &PeriodicLattice) -> DMatrix<u8> {
 
     mat
 }
+
+pub fn get_filled(latt: &PeriodicLattice) -> DMatrix<u8> {
+    DMatrix::repeat(latt.system_size, latt.system_size, 1)
+}
+
+pub fn get_horizontal_stripe(latt: &PeriodicLattice) -> DMatrix<u8> {
+    DMatrix::from_fn(latt.system_size, latt.system_size, |row, _| (row%2) as u8)
+}
