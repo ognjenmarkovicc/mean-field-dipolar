13e8f8ee183ee719d70c4046c631e55ea31755b5
ognjenmarkovicc <ognjen.markovicc@gmail.com>
2023-04-01 12:53:58 -0400

    Add pattern selection from a config file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate nalgebra as na;
 extern crate serde_json;
+extern crate toml;
+
 use mean_field_dipolar::dipolar::{simulation_sweep, Pattern};
 use mean_field_dipolar::lattice::get_checkerboard;
 use mean_field_dipolar::util;
@@ -7,13 +9,36 @@ use na::DVector;
 use std::f64::consts::PI;
 use std::path::Path;
 use std::time::Instant;
+use std::fs;
+use serde::Deserialize;
 
+
+#[derive(Deserialize)]
+struct Config {
+    /// pattern to simulate
+   pattern: String,
+}
 
 fn main() {
     // save path
     let save_path = Path::new("./");
+    let config_path = Path::new("./sim.toml");
 
-    let patt = Pattern::HStripe;
+    // read the config
+    let config_str = fs::read_to_string(config_path)
+        .expect("Config file wasn't found");
+
+    let config: Config = toml::from_str(config_str.as_ref())
+        .expect("Failed reading config file");
+
+    let patt = match config.pattern.to_lowercase().as_ref() {
+        "filled" => Pattern::Filled,
+        "cb" => Pattern::CB,
+        "hstripe" => Pattern::HStripe,
+        _ => Pattern::Filled
+    };
+
+    println!("Selected pattern {:?}", patt);
 
     simulation_sweep(save_path, &patt, (1, 2), (4, 6), PI/2., 0., 20.);
 }
